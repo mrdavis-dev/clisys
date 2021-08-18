65109ac2638c43d15f649cedfb9eedd55365b84d
David Flores <31556253+mrdavis-dev@users.noreply.github.com>
2021-08-18 02:54:23 -0500

    upload

--- admin/conexion/config.php
+++ admin/conexion/config.php
@@ -1,6 +1,6 @@
 <?php
    define('DB_SERVER', '172.17.0.7');
-   define('DB_USERNAME', 'root');
+   define('DB_USERNAME', 'userroot');
    define('DB_PASSWORD', '#dbcontainer.com');
    define('DB_DATABASE', 'anguizola');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
